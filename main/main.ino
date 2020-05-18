b05b546713398420d66e77e63eb137307994ab3c
ssraf <rafaelsanchezsouza@gmail.com>
2020-05-18 10:26:14 -0300

    refatoracao do codigo funcionando

---
+++ main/main.ino
@@ -0,0 +1,39 @@
+//**************************************************************************
+//-------------- Make Jampa @ João Pessoa - Paraíba - Brazil --------------
+//
+// Version: 1.0
+// Date: 18.05.2020 [mm.dd.yyyy]
+// Description
+//  MQTT Communication
+//  Current Reading
+//  ESP8266 + SCT013
+// Input files:
+// (None)
+//
+// Final Output files:
+//   (None)
+//
+// References
+//  https://learn.openenergymonitor.org/
+//  https://techtutorialsx.com/2017/04/09/esp8266-connecting-to-mqtt-broker/
+//  https://www.electronicwings.com/nodemcu/nodemcu-adc-with-arduino-ide
+//  
+//**************************************************************************
+// Steps
+//**************************************************************************
+
+#include "iotpb.h"
+
+void setup() 
+{
+  setupIoT();
+}
+
+void loop()
+{
+  loopIoT();
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
